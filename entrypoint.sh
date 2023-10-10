433f0cae721d9a1a4dc3af24940c99df3e817264
MaximMatveevOneUp <129727979+MaximMatveevOneUp@users.noreply.github.com>
2023-10-10 14:01:55 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,10 @@ FILES=${INPUT_PATH}
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 
-echo "START"
-echo $FILES
 for FILE in "$FILES"/*
 do
 	if [ -f "$FILE" ]; then		
 		result=$(xmllint --xpath 'string(/test-run/@result)' "$FILE")	
-		echo $result
 		if [ "$result" != "Passed" ]; then
 			echo -e "${RED}$(basename -- "${FILE%.*}") $result" >> GITHUB_STEP_SUMMARY
 		    echo -e "${RED}$(xmllint --xpath 'string(/test-run/*//test-case/@fullname)' "$FILE")" >> GITHUB_STEP_SUMMARY
@@ -20,5 +17,3 @@ do
 		fi
 	fi
 done
-
-echo "END"
