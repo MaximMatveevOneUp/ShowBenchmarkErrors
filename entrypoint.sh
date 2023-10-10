b00663e94663c0d405da35dd65a27b705bfaf467
MaximMatveevOneUp <129727979+MaximMatveevOneUp@users.noreply.github.com>
2023-10-10 19:18:28 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,16 +6,14 @@ GREEN='\033[0;32m'
 
 for FILE in "$FILES"/*
 do
-	echo $FILE
 	if [ -f "$FILE" ]; then		 		
 		result=$(xmllint --xpath 'string(/test-run/@result)' "$FILE")	
-  		echo $result
 		if [ "$result" != "Passed" ]; then
-			echo -e "${RED}$(basename -- "${FILE%.*}") $result" >> GITHUB_STEP_SUMMARY
-		    echo -e "${RED}$(xmllint --xpath 'string(/test-run/*//test-case/@fullname)' "$FILE")" >> GITHUB_STEP_SUMMARY
-		    echo -e "${RED}$(xmllint --xpath 'string(/test-run/*//output)' "$FILE")" >> GITHUB_STEP_SUMMARY
+			echo -e "${RED}$(basename -- "${FILE%.*}") $result" >> $GITHUB_STEP_SUMMARY
+			echo -e "${RED}$(xmllint --xpath 'string(/test-run/*//test-case/@fullname)' "$FILE")" >> $GITHUB_STEP_SUMMARY
+			echo -e "${RED}$(xmllint --xpath 'string(/test-run/*//output)' "$FILE")" >> $GITHUB_STEP_SUMMARY
 		else
-			echo -e ${GREEN}$(basename -- "${FILE%.*}") $result >> GITHUB_STEP_SUMMARY
+			echo -e ${GREEN}$(basename -- "${FILE%.*}") $result >> $GITHUB_STEP_SUMMARY
 		fi
 	fi
 done
